89c3305c35101a96bb7ef042bbf34b752d018e42
Sergei-Eremin <serezhanv@gmail.com>
2022-08-05 13:57:38 +0300

    fix: fix appenden control

--- src/components/control/control.ts
+++ src/components/control/control.ts
@@ -5,7 +5,7 @@ export class Control {
   generateControl(race: Race, carTrack: CarTrack) {
     const field = document.createElement('div');
     field.classList.add('fields');
-    (document.querySelector('.app') as HTMLElement).after(field);
+    (document.querySelector('.app') as HTMLElement).append(field);
     const arr: string[] = [carTrack.createInputs(), carTrack.updateInputs(), race.generateButtons()];
     field.innerHTML = `${arr.map((elem) => elem).join('')}`;
   }
